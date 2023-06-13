751ec629c2ac7a74c2c314af80c854bfd44c0d6d
Geoff Hackett <geoff.hackett@reddit.com>
2023-06-13 09:53:19 -0400

    Remove constructors

--- src/test/kotlin/SampleTest.kt
+++ src/test/kotlin/SampleTest.kt
@@ -3,8 +3,8 @@ import io.mockk.mockk
 import io.mockk.slot
 import org.junit.Test
 
-class Impl1(val name: String)
-class Impl2(val name: String)
+class Impl1
+class Impl2
 
 interface InterfaceToMock {
     fun <OP> doThing(op: OP): OP
@@ -15,26 +15,26 @@ class SampleTest {
     private val thing: InterfaceToMock = mockk()
 
     @Test fun sampleTest_fails() {
-        every { thing.doThing(any<Impl1>()) } returns Impl1("1")
-        every { thing.doThing(any<Impl2>()) } returns Impl2("2")
+        every { thing.doThing(any<Impl1>()) } returns Impl1()
+        every { thing.doThing(any<Impl2>()) } returns Impl2()
 
-        val result1 = thing.doThing(Impl1("op1"))
-        val result2 = thing.doThing(Impl2("op2"))
+        val result1 = thing.doThing(Impl1())
+        val result2 = thing.doThing(Impl2())
     }
 
     @Test fun sampleTest_succeeds() {
-        every { thing.doThing(any<Impl1>()) } returns Impl1("1")
-        every { thing.doThing(any<Impl2>()) } returns Impl2("2")
+        every { thing.doThing(any<Impl1>()) } returns Impl1()
+        every { thing.doThing(any<Impl2>()) } returns Impl2()
 
-        thing.doThing(Impl1("op1"))
-        thing.doThing(Impl2("op2"))
+        thing.doThing(Impl1())
+        thing.doThing(Impl2())
     }
 
     @Test fun sampleTest_succeeds_with_slots() {
-        every { thing.doThing(capture(slot<Impl1>())) } returns Impl1("1")
-        every { thing.doThing(capture(slot<Impl2>())) } returns Impl2("2")
+        every { thing.doThing(capture(slot<Impl1>())) } returns Impl1()
+        every { thing.doThing(capture(slot<Impl2>())) } returns Impl2()
 
-        val result1 = thing.doThing(Impl1("op1"))
-        val result2 = thing.doThing(Impl2("op2"))
+        val result1 = thing.doThing(Impl1())
+        val result2 = thing.doThing(Impl2())
     }
 }
